f21c5c4075e85b0f96badd95d4257245e13b616e
LilySny <liviacastilholi@gmail.com>
2022-06-20 01:23:00 -0300

    removed unecessary var from test

--- test/home/domain/usecases/find_all_users_test.dart
+++ test/home/domain/usecases/find_all_users_test.dart
@@ -5,32 +5,13 @@ import 'package:mobile_challenge/home/data/models/user_model.dart';
 import 'package:mobile_challenge/home/domain/usecases/find_all_users.dart';
 import 'package:mockito/mockito.dart';
 
+import '../../mocks/github_json_mock.dart';
 import '../../mocks/mock_github_service.dart';
 
 void main() {
   FindAllUsers usecase;
   MockGithubService mockGithubService;
-  final _mockResult = '''{
-   "data":[
-      {
-         "login":"LilySny",
-         "id":33629714,
-         "avatar_url":"https://avatars.githubusercontent.com/u/33629714?v=4",
-         "location":"Campinas, São Paulo - Brazil",
-         "email":"liviacastilholi@gmail.com",
-         "bio":""
-      },
-      {
-         "login":"LilySny2",
-         "id":33629715,
-         "avatar_url":"https://avatars.githubusercontent.com/u/33629714?v=4",
-         "location":"Campinas, São Paulo - Brazil",
-         "email":"liviacastilholi2@gmail.com",
-         "bio":""
-      }
-   ]
-} ''';
-
+ 
   setUp(() {
     mockGithubService = MockGithubService();
     usecase = FindAllUsers(mockGithubService);
@@ -39,7 +20,7 @@ void main() {
   test(
     'should get users in the repository',
     () async {
-      final data = jsonDecode(_mockResult)["data"] as List;
+      final data = jsonDecode(mockUserList)["data"] as List;
       final response = data.map((e) => UserModel.fromJson(e)).toList();
       when(mockGithubService.findAll("Lily"))
           .thenAnswer((_) async => response);

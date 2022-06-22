c51e0580732972b2be1a10a69d1210080464a706
LilySny <liviacastilholi@gmail.com>
2022-06-22 00:17:53 -0300

    add test group in github service test

--- test/home/data/github_service_test.dart
+++ test/home/data/github_service_test.dart
@@ -25,42 +25,44 @@ void main() {
     dio.close();
   });
 
-  test(
-    'should get users if status is 200',
-    () async {
-      dioAdapter.onGet(
-        path,
-        (server) => server.reply(
-          200,
-          mockUserList,
-          delay: const Duration(seconds: 1),
-        ),
-      );
+  group('github - user service tests', () {
+    test(
+      'should get users if status is 200',
+      () async {
+        dioAdapter.onGet(
+          path,
+          (server) => server.reply(
+            200,
+            mockUserList,
+            delay: const Duration(seconds: 1),
+          ),
+        );
 
-      final response = await dio.get(path);
+        final response = await dio.get(path);
 
-      final data = json.decode(response.data)["data"] as List;
-      final List<User> list = data.map((e) => UserModel.fromJson(e)).toList();
+        final data = json.decode(response.data)["data"] as List;
+        final List<User> list = data.map((e) => UserModel.fromJson(e)).toList();
 
-      expect(list.first.email, contains('livia'));
-    },
-  );
+        expect(list.first.email, contains('livia'));
+      },
+    );
 
-  test(
-    'should throw error if status is different from 200',
-    () async {
-      dioAdapter.onGet(
-        path,
-        (server) => server.throws(
-          500,
-          DioError(requestOptions: RequestOptions(path: path)),
-          delay: const Duration(seconds: 1),
-        ),
-      );
+    test(
+      'should throw error if status is different from 200',
+      () async {
+        dioAdapter.onGet(
+          path,
+          (server) => server.throws(
+            500,
+            DioError(requestOptions: RequestOptions(path: path)),
+            delay: const Duration(seconds: 1),
+          ),
+        );
 
-      final response = service.findAll('lily');
+        final response = service.findAll('lily');
 
-      await expectLater(response, throwsA((e) => e is DioError));
-    },
-  );
+        await expectLater(response, throwsA((e) => e is DioError));
+      },
+    );
+  });
 }
